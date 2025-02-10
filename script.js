882f7fd2f944cdceebf5a957cce148c70c4d004b
Ntkitae <lykov.nik.ebk.219@gmail.com>
2025-02-10 23:45:18 +0300

    правки

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function generateRandomNumber(){
     return Math.floor(Math.floor(Math.random) * 100) + 1;
 }
-let randomNumber = generateRandomNumber();
+
 function playGame(){
-let UserNumber = null;
-while(UserNumber!==randomNumber){    
-UserNumber= Namber(prompt('Веди число от 1 до 100'));
-if (Number(UserNumber)>randomNumber) {
-    alert('Ваше число больше');
-} else if(Number(UserNumber)<randomNumber) {
-    alert('Ваше число меньше');
-} else{
-    alert('Вы угадали');
-}
-}
+   let UserNumber = null;
+   let randomNumber = generateRandomNumber();
+     
+   while(UserNumber!==randomNumber){    
+      UserNumber= Number(prompt('Веди число от 1 до 100'));
+
+       if (UserNumber > randomNumber) {
+          alert('Ваше число больше');
+       } else if(Number(UserNumber) < randomNumber) {
+          alert('Ваше число меньше');
+       } else{
+          alert('Вы угадали');
+       }
+    }
 } 
 playGame();
