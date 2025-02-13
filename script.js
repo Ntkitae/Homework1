e9dc65280715be1f296f365c6d9233403e5eec40
Ntkitae <lykov.nik.ebk.219@gmail.com>
2025-02-13 16:29:24 +0300

    выполненое дз на проверку

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+
 function generateRandomNumber(){
-    return Math.floor(Math.floor(Math.(random)) * 100) + 1;
+    return Math.floor(Math.random() * 100) + 1;
 }
 
 function playGame(){
-   let UserNumber = null;
-   let randomNumber = generateRandomNumber();
-     
-   while(UserNumber!==randomNumber){    
-      UserNumber= Number(prompt('Веди число от 1 до 100'));
+   let userNumber = null;
+   let randomNumber = generateRandomNumber(); 
+   while(userNumber!==randomNumber){    
+      userNumber = Number(prompt('Веди число от 1 до 100'));
 
-       if (UserNumber > randomNumber) {
+       if (userNumber > randomNumber) {
           alert('Ваше число больше');
-       } else if(UserNumber < randomNumber) {
+       } else if(userNumber < randomNumber) {
           alert('Ваше число меньше');
        } else{
           alert('Вы угадали');
        }
     }
 } 
-playGame();
+
