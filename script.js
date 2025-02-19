d4f786992c98ce524aaa95cdba4ab63d57fdf4fb
Ntkitae <lykov.nik.ebk.219@gmail.com>
2025-02-19 22:26:42 +0300

    задача по js

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ function playGame(){
     }
 } 
 
+function generateRandomNumbers(){
+   const number1 = Math.floor(Math.random() * 10) + 1; 
+   const number2 = Math.floor(Math.random() * 10) + 1; 
+   return [number1, number2];
+}
+console.log(generateRandomNumbers())
+
+    
+function arifmetic(){
+
+   const operators = ['+', '-', '*', '/'];
+   let operator = operators[Math.floor(Math.random() * operators.length)];
+
+   let a = Math.floor(Math.random() * 10) + 1; 
+   let b  = Math.floor(Math.random() * 10) + 1;
+
+   const task = `${a}, ${operator}, ${b}`;
+   return task;
+   
+   userAnswer = prompt('task');
+
+
+
+
+
+}
